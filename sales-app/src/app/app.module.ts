cd9ebfff6bd89d9d9e2b1a5a1cfecd76fff0252a
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-05-26 17:42:50 -0300

    adicionado o component pagina inicial

--- sales-app/src/app/app.module.ts
+++ sales-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { PaginaInicialComponent } from './content/pagina-inicial/pagina-inicial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    PaginaInicialComponent
   ],
   imports: [
     BrowserModule,
